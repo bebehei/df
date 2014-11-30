be4ba807973e5776ca2044d643fd599dce4b4140
Benedikt Heine <bebe@bebehei.de>
2014-11-30 01:14:14 +0100

    adding deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+DEPLOY="./deployment"
+
+updLink(){
+	# if file is a symbolic link, remove the old one
+	[[ -L $1 ]] && rm $1
+	ln -s $1 $2
+}
+
+cat $DEPLOY | while read line; do
+	src=$(echo $line | cut -d ' ' -f 1)
+	dst=$(echo $line | cut -d ' ' -f 2)
+
+	# make a difference if destination has got leading slash
+	#    leading slash -> absolute link
+	# no leading slash -> link based on $HOME 
+	if [[ "$dst" =~ ^/ ]]; then
+		updLink $(pwd)/$src $dst
+		else
+			updLink $(pwd)/$src $HOME/$dst
+	fi
+done
