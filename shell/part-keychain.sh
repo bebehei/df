505284dc657a410382fb66520a93805f2de84c82
Benedikt Heine <bebe@bebehei.de>
2015-03-10 19:16:40 +0100

    rewrite shell/keychain

--- shell/part-keychain.sh
+++ shell/part-keychain.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
-KEYFOLDERS+=('~/.ssh/keys')
+# array to process, either folders or directories for recursive processing
+KEYPATHS+=("$HOME/.ssh/keys" "$HOME/.ssh/id_rsa")
 
-for folder in ${KEYFOLDERS[@]}; do
-    if [ -d $folder ]; then
-        for key in `find ~/.ssh/keys -type f -not -name '*.pub'`; do
+for folder in ${KEYPATHS[@]}; do
+    if [ -f "$folder" ]; then
+        eval `keychain -q --agents ssh --timeout 600 --eval $folder`
+    fi
+
+     if [ -d $folder ]; then
+        for key in `find $folder -type f -not -name '*.pub'`; do
             eval `keychain -q --agents ssh --timeout 600 --eval $key`
         done
-    fi
+     fi
 done
