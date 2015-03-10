d5933181e8c8114e44176026e14fd3be46ce4a44
Benedikt Heine <bebe@bebehei.de>
2015-03-10 17:18:20 +0100

    make keychain processing multiple folders and check for existence first

--- shell/part-keychain.sh
+++ shell/part-keychain.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-for key in `find ~/.ssh/keys -type f -not -name '*.pub'`; do
-    eval `keychain -q --agents ssh --timeout 600 --eval $key`
+KEYFOLDERS+=('~/.ssh/keys')
+
+for folder in ${KEYFOLDERS[@]}; do
+    if [ -d $folder ]; then
+        for key in `find ~/.ssh/keys -type f -not -name '*.pub'`; do
+            eval `keychain -q --agents ssh --timeout 600 --eval $key`
+        done
+    fi
 done
