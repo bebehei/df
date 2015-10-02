56ed566af927e4e4c308905bdc427a05dd1e0c35
Benedikt Heine <bebe@bebehei.de>
2015-10-02 15:03:09 +0200

    [keychain] speedup and use only one keychain call

--- shell/part-keychain.sh
+++ shell/part-keychain.sh
@@ -1,16 +1,7 @@
 #!/bin/sh
 
 # array to process, either folders or directories for recursive processing
-KEYPATHS+=("$HOME/.ssh/keys" "$HOME/.ssh/id_rsa")
+KEYPATHS+=("$HOME/.ssh/keys")
 
-for folder in ${KEYPATHS[@]}; do
-    if [ -f "$folder" ]; then
-        eval `keychain -q --agents ssh --timeout 600 --eval $folder`
-    fi
-
-     if [ -d $folder ]; then
-        for key in `find $folder -type f -not -name '*.pub'`; do
-            eval `keychain -q --agents ssh --timeout 600 --eval $key`
-        done
-     fi
-done
+eval `find ${KEYPATHS[@]} -type f -not -name '*.pub' | xargs keychain -q --nogui --agents ssh --timeout 600 --eval`
+unset KEYPATHS
