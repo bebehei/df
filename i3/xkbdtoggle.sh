fc05c06e77ee34c2ff6571dbda9f1413c9adf151
Benedikt Heine <bebe@bebehei.de>
2015-10-08 00:47:23 +0200

    [i3] rewrite xkbtoggle

--- i3/xkbdtoggle.sh
+++ i3/xkbdtoggle.sh
@@ -1,24 +1,15 @@
 #!/usr/bin/env bash
 
+# Q: But you could've solved it with an easy option in your X.org server!
+# A: Yes, but I want to bootstrap my repository without root access.
+
 KBDLIST=$HOME/.i3/kbdlist
 
-KBDFH=$HOME/.i3/layout_now
-KBDNOW=`cat $KBDFH`
-KBDNEXT=`head -n 1 $KBDLIST`
+KBDNOW=$(setxkbmap -query | awk '/^layout:/ { print $2}')
 
-savelay=0
-for kbd in $(cat $KBDLIST); do
-	if [[ "$savelay" != 0 ]]; then
-		KBDNEXT=$kbd
-		savelay=0
-		break
-	fi
-
-	if [[ "$KBDNOW" = "$kbd" ]]; then
-		savelay=1
-	fi
-done
-
-setxkbmap $KBDNEXT
-echo layout is now $KBDNEXT
-echo $KBDNEXT > $KBDFH;
+cat $KBDLIST $KBDLIST \
+	| grep -A 1 "^$KBDNOW$" \
+	| grep -v $KBDNOW \
+	| cat - $KBDLIST \
+	| head -n 1 \
+	| xargs setxkbmap
