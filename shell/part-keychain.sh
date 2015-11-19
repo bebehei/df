2b82f62d32c2c3b851bc2b1fa8e6c410df96c13f
Benedikt Heine <bebe@bebehei.de>
2015-11-19 12:25:53 +0100

    make find not complain about non-existent paths

--- shell/part-keychain.sh
+++ shell/part-keychain.sh
@@ -3,5 +3,5 @@
 # array to process, either folders or directories for recursive processing
 KEYPATHS+=("$HOME/.ssh/keys")
 
-eval `find ${KEYPATHS[@]} -type f -not -name '*.pub' | xargs keychain -q --nogui --agents ssh --timeout 600 --eval`
+eval `find ${KEYPATHS[@]} -type f -not -name '*.pub' 2>/dev/null | xargs keychain -q --nogui --agents ssh --timeout 600 --eval`
 unset KEYPATHS
