2ee4e095465538c4c2c2be1ca48eeeb8377289ca
Felix Buehler <felix.christian.buehler@web.de>
2016-02-03 22:25:39 +0100

    [shell] changed to my defaults

--- shell/part-keychain.sh
+++ shell/part-keychain.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # array to process, either folders or directories for recursive processing
-KEYPATHS+=("$HOME/.ssh/keys")
+KEYPATHS+=("$HOME/.ssh/keys" "$HOME/.ssh/gitlab_rsa" "$HOME/.ssh/id_rsa")
 
 eval `find ${KEYPATHS[@]} -type f -not -name '*.pub' 2>/dev/null | xargs keychain -q --nogui --agents ssh --timeout 600 --eval`
 unset KEYPATHS
