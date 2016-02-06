269c8b9cd851647277377565cd05cc6a6d59893d
Benedikt Heine <bebe@bebehei.de>
2016-02-06 15:55:23 +0100

    [shell/gitrepo] make .configrepo link unneccessary

--- shell/part-gitrepo-commited.sh
+++ shell/part-gitrepo-commited.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
-#check if git-repo has new entries
-if [ ! -f $HOME/.configrepo ]; then
-    echo ".configrepo missing!"
-    return 1
-fi
-cfgrepo=`readlink $HOME/.configrepo`
+
+cfgrepo=`readlink $0`
 repo=`dirname $cfgrepo`
-changes=`cd $repo && git status --porcelain | wc -l`
+changes=`git -C $repo status --porcelain | wc -l`
 
 if [ $changes -gt 0 ]; then
 	echo -e '\e[01;31m'
