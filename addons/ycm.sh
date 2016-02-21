18b22daf2972c2770d7493a9bfb2ed630dcca001
Benedikt Heine <bebe@bebehei.de>
2016-02-22 00:21:03 +0100

    [addons/ycm] add YouCompleteMe script

---
+++ addons/ycm.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# commands needed:
+# cargo
+# npm
+
+# This will install YouCompleteMe to vim
+# parts of the program are taken from the official Installation Instructions
+
+mkdir -p ~/.vim/bundle-external
+cd ~/.vim/bundle-external
+
+git clone https://github.com/Valloric/YouCompleteMe.git
+cd YouCompleteMe
+git submodule update --init --recursive
+
+PYTHON_BINARY=python
+if command -v python2 2>&1 >/dev/null; then
+  PYTHON_BINARY=python2
+fi
+
+$PYTHON_BINARY ./install.py --clang-completer --omnisharp-completer --gocode-completer --tern-completer --racer-completer
