ba4d259ad1571324a56992f54557b16381cb6b39
Benedikt Heine <bebe@bebehei.de>
2016-06-28 12:03:01 +0200

    [shell/gitrepo] only print information, if in interactive shell

--- shell/part-gitrepo-commited.sh
+++ shell/part-gitrepo-commited.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
-cfgrepo=`readlink ~/.profile.d/gitrepo-commited.sh`
-repo=`dirname $cfgrepo`
-changes=`git -C $repo status --porcelain | wc -l`
+case "$-" in
+	*i*)
+		# This shell is interactive, if not ignore this part
+		# If printing this in non-interactive shells, it'll may prevent scp to work
+		cfgrepo=`readlink ~/.profile.d/gitrepo-commited.sh`
+		repo=`dirname $cfgrepo`
+		changes=`git -C $repo status --porcelain | wc -l`
 
-if [ $changes -gt 0 ]; then
-	echo -e '\e[01;31m'
-	echo '####################################'
-	echo '# GIT-config-Repo is not commited! #'
-	echo '####################################'
-	echo -e '\e[0m'
-fi
+		if [ $changes -gt 0 ]; then
+			echo -e '\e[01;31m'                            >&2
+			echo -e '####################################' >&2
+			echo -e '# GIT-config-Repo is not commited! #' >&2
+			echo -e '####################################' >&2
+			echo -e '\e[0m'                                >&2
+		fi
+		;;
+esac
