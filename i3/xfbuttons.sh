c21a9bf43af37257c2400ed9724dc0bb0d175755
Benedikt Heine <bebe@bebehei.de>
2016-07-11 15:54:00 +0200

    [xfbuttons] add VLC to control

--- i3/xfbuttons.sh
+++ i3/xfbuttons.sh
@@ -3,21 +3,25 @@
 case $1 in
 	XF86AudioPlay)
 		xdotool key --window $(xdotool search --limit 1 --name "Spotify (Premium )?- Linux Preview") $1
+		xdotool key --window $(xdotool search --limit 1 --name ".*VLC media player") $1
 		dbus-send --type=method_call --dest=org.mpris.MediaPlayer2.rhythmbox  /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause
 	;;
 		
 	XF86AudioNext)
 		xdotool key --window $(xdotool search --limit 1 --name "Spotify (Premium )?- Linux Preview") $1
+		xdotool key --window $(xdotool search --limit 1 --name ".*VLC media player") $1
 		dbus-send --type=method_call --dest=org.mpris.MediaPlayer2.rhythmbox  /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next
 	;;
 		
 	XF86AudioPrev)
 		xdotool key --window $(xdotool search --limit 1 --name "Spotify (Premium )?- Linux Preview") $1
+		xdotool key --window $(xdotool search --limit 1 --name ".*VLC media player") $1
 		dbus-send --type=method_call --dest=org.mpris.MediaPlayer2.rhythmbox  /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous
 	;;
 
 	XF86AudioPrev)
 		xdotool key --window $(xdotool search --limit 1 --name "Spotify (Premium )?- Linux Preview") $1
+		xdotool key --window $(xdotool search --limit 1 --name ".*VLC media player") $1
 		dbus-send --type=method_call --dest=org.mpris.MediaPlayer2.rhythmbox  /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous
 	;;
 	XF86AudioRaiseVolume)
