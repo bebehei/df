388d1ca56041700a6e22bf730e4888f5b859d76a
Benedikt Heine <bebe@bebehei.de>
2016-08-10 23:17:37 +0200

    [shell/keychain] make script more readable

--- shell/part-keychain.sh
+++ shell/part-keychain.sh
@@ -3,5 +3,9 @@
 # array to process, either folders or directories for recursive processing
 KEYPATHS+=("$HOME/.ssh/keys")
 
-command -v keychain 2>&1 >/dev/null && eval `find ${KEYPATHS[@]} -type f -not -name '*.pub' 2>/dev/null | xargs keychain -q --nogui --agents ssh --timeout 600 --eval`
+if command -v keychain 2>&1 >/dev/null; then
+	eval \
+		`find ${KEYPATHS[@]} -type f -not -name '*.pub' 2>/dev/null \
+		| xargs keychain -q --nogui --agents ssh --timeout 600 --eval`
+fi
 unset KEYPATHS
