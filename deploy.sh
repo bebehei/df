2d582d131d55592faf4cb97d150872b1b6e342aa
Benedikt Heine <bebe@bebehei.de>
2016-08-10 23:10:35 +0200

    add stale link checker to prevent bad usage of this deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,9 @@
 
 BASE=$(dirname $(readlink -f $0))
 
+error(){ echo -e "\e[31m$*\e[0m"; }
+warn(){  echo -e "\e[33m$*\e[0m"; }
+
 # You can use another base or file by setting the
 # environment DEPL_BASE or DEPL_FILE
 DEPL_BASE=${DEPL_BASE:-$BASE}
@@ -23,3 +26,29 @@ grep -v --perl-regexp '^\s*#' $DEPL_FILE | while read line; do
 	[[ -L $dst ]] && rm $dst
 	ln -s $BASE/$src $dst
 done
+
+git -C $DEPL_BASE log -p $DEPL_FILE \
+	| sed -n -e '/^+++/d' -e '/^+\s*#/d' -e 's/^\+//p' \
+	| sort -u \
+	| comm -23 - <(grep -v --perl-regexp '^\s*#' $DEPL_FILE | sort -u) \
+	| while read line; do
+		src=$(echo "${line//\~/$HOME}" | awk '{print $1}')
+		dst=$(echo "${line//\~/$HOME}" | awk '{print $2}')
+
+
+		if [[ "$(readlink -f $BASE/$src)" == "$(readlink -f $dst)" ]]; then
+			if [ "x$print_header" == "x" ]; then
+				echo "Stale Link Checker"
+				echo "It's a good practice to remove doubled and stale links."
+				print_header=printed
+			fi
+			error "Doubled link: '$dst'"
+		elif [ -L "$dst" ]; then
+			if [ "x$print_header" == "x" ]; then
+				echo "Stale Link Checker"
+				echo "It's a good practice to remove doubled and stale links."
+				print_header=printed
+			fi
+			warn "Stale link: '$dst'"
+		fi
+	done
