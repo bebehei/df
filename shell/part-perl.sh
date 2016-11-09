64407b7c7d268581d95bcbf5eb7606819b3b97c1
Benedikt Heine <bebe@bebehei.de>
2016-11-09 15:23:53 +0100

    [shell] move language-specific path-files into single one

--- shell/part-perl.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# PERL cpan modules
-export PATH="$HOME/perl5/bin:$PATH"
-export PERL5LIB="$HOME/perl5/lib/perl5:$PERL5LIB"
-export PERL_LOCAL_LIB_ROOT="$HOME/perl5:$PERL_LOCAL_LIB_ROOT"
-export PERL_MB_OPT="--install_base \"$HOME/perl5\""
-export PERL_MM_OPT="INSTALL_BASE=$HOME/perl5"
--- shell/part-ruby.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#rubygems path
-if which ruby 2>&1 >/dev/null && which gem 2>&1 >/dev/null; then
-    PATH="$(ruby -rubygems -e 'puts Gem.user_dir')/bin:$PATH"
-fi
