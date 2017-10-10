abf9896e7af7b72e95de9b46f48b9c7248d55ed8
Benedikt Heine <bebe@bebehei.de>
2017-10-10 16:06:48 +0200

    Add dunst aliases

--- shell/part-aliases.sh
+++ shell/part-aliases.sh
@@ -98,3 +98,33 @@ dpms(){
 # - xargs it to nmap at the end
 
 alias snet="ip addr | \\grep -v "inet6" | \\grep inet | cut -d \" \" -f 6 | \\grep -v '127\\.0\\.[0-1]\\.[0-1]' | xargs -n 1 ipcalc | awk '/Network:/{print \$2}' | sort -u | xargs nmap"
+
+
+DUNST_REPO=~/code/dunst
+# Clone dunst into these paths to make it work
+#git clone git@github.com:dunst-project/dunst.git $DUNST_REPO
+# suffix the repos with .o to hide them from git, prefix with . to hide from myself
+#git clone git@github.com:dunst-project/dunst-project.org.git $DUNST_REPO/.dunst-project.o
+#git clone https://aur.archlinux.org/dunst-git.git $DUNST_REPO/.dunst-git.o
+
+# Dunst specific aliases
+alias dmake="make -j4 -C $DUNST_REPO"
+alias drun="killall dunst; $DUNST_REPO/dunst -conf $DUNST_REPO/.testrc.o"
+alias dgdb="killall dunst; gdb -ex run ${DUNST_REPO}/dunst -ex 'set confirm off' -ex bt"
+alias dtest="killall dunst; dmake clean && dmake all test; gdb -cd ${DUNST_REPO}/test -ex run ${DUNST_REPO}/test/test -ex 'set confirm off' -ex bt"
+alias dvalgrind="killall dunst; valgrind --leak-check=full --show-leak-kinds=definite --track-origins=yes ${DUNST_REPO}/dunst -startup_notification yes"
+alias dhugo="(cd ${DUNST_REPO}/.dunst-project.o && hugo server) &; xdg-open http://localhost:1313/"
+
+# Build the dunst package and run it directly
+
+function dpkg(){
+	rm -rf ${DUNST_REPO}/.dunst-git/dunst # force a reclone
+
+	pushd ${DUNST_REPO}/.dunst-git.o
+	sed -i "s%^source=.*%source=('git+file://${DUNST_REPO}#branch=${1:-master}')%" PKGBUILD
+	makepkg -f
+	sudo pacman -U --noconfirm $(ls -c dunst-*.pkg.tar.xz | head -n 1)
+	popd
+
+	killall dunst
+}
