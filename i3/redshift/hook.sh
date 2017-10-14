b82911d40b6216624e2c62b37989b12d808fb4ba
Benedikt Heine <bebe@bebehei.de>
2017-10-14 18:44:28 +0200

    [redshift] use icons and transient hint for notifications

--- i3/redshift/hook.sh
+++ i3/redshift/hook.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
+notify(){
+	notify-send \
+		--app-name=redshift \
+		--urgency=low \
+		--icon=redshift \
+		--hint=int:transient:1 \
+		"${1}" "${2}"
+}
+
 case $1 in
 	period-changed)
-		exec notify-send "Redshift" "Period changed to $3"
+		notify "Redshift" "Period changed to $3"
 		;;
 	*)
-		exec notify-send "Redshift" "Redshift just fired an unknown hook with these options: '$*'. Ah, of course, this notification is just here, to troll you! :-P"
+		notify "Redshift" "Redshift just fired an unknown hook with these options: '$*'. Ah, of course, this notification is just here, to troll you! :-P"
 		;;
 esac
