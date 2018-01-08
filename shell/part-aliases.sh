181f4f91aa62f02a64c16c9a8efc5983c24aad1c
Benedikt Heine <bebe@bebehei.de>
2018-01-08 14:16:44 +0100

    Execute backup as root by default

--- shell/part-aliases.sh
+++ shell/part-aliases.sh
@@ -46,7 +46,7 @@ alias of="xdg-open"
 
 if [ ! -z "$(type sudo 2>/dev/null)" -a "$USER" != 'root' ]; then
 	#create sudo aliases WITHOUT leading s
-	for sudo in mount umount iftop iotop fsadm lvchange lvconvert lvcreate lvdisplay lvextend lvm lvmchange lvmconf lvmconfig lvmdiskscan lvmdump lvmetad lvmsadc lvmsar lvreduce lvremove lvrename lvresize lvs lvscan pvchange pvck pvcreate pvdisplay pvmove pvremove pvresize pvs pvscan vgcfgbackup vgcfgrestore vgchange vgck vgconvert vgcreate vgdisplay vgexport vgextend vgimport vgimportclone vgmerge vgmknodes vgreduce vgremove vgrename vgs vgscan vgsplit;
+	for sudo in mount umount iftop iotop fsadm lvchange lvconvert lvcreate lvdisplay lvextend lvm lvmchange lvmconf lvmconfig lvmdiskscan lvmdump lvmetad lvmsadc lvmsar lvreduce lvremove lvrename lvresize lvs lvscan pvchange pvck pvcreate pvdisplay pvmove pvremove pvresize pvs pvscan vgcfgbackup vgcfgrestore vgchange vgck vgconvert vgcreate vgdisplay vgexport vgextend vgimport vgimportclone vgmerge vgmknodes vgreduce vgremove vgrename vgs vgscan vgsplit backup;
 	do
 		type $sudo > /dev/null 2>&1 && alias $sudo="sudo $sudo";
 	done
