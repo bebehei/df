0bb68a058a14db94e81defb3b02d0be8f0aa0055
Benedikt Heine <bebe@bebehei.de>
2018-03-19 18:08:51 +0100

    Support primary monitor setting with xrandr
    This circumvents issues with applications having their properties bound
    to specifics of the monitor.
    
    - dunst has always shown its notifications on the left side (if no
      window opened and no primary monitor set)
      See dunst-project/dunst#479
    - i3bar tray had been always on the right side
    
    Setting proactively the primary monitor and binding i3bar, fixes the
    weird positioning issue.

--- i3/xmon.sh
+++ i3/xmon.sh
@@ -1,32 +1,23 @@
 #!/bin/sh
 
-die(){
-	echo $* >&2
-	exit 1
-}
+die(){ echo $* >&2; exit 1; }
 
 profile=$1
 profiledir=~/.display
 
 [ -n "$profile" ] || die "No profile specified."
 [ -f "$profiledir/$profile.cfg" ] || die "No profile '$profile' found."
-randrargs=""
 
-while read line; do
-	output=`echo $line | awk '{print $1}'`
-	  mode=`echo $line | awk '{print $2}'`
-	   pos=`echo $line | awk '{print $3}'`
-	rotate=`echo $line | awk '{print $4}'`
+grep -v '^\s*#' $profiledir/$profile.cfg \
+	| while read output mode pos rotate primary; do
 
-	if [[ "$mode" == off ]]; then
-		randrargs="$randrargs --output $output --off"
-	elif [ -z "$rotate" ]; then
-		randrargs="$randrargs --output --mode $mode --pos $pos"
+	echo "--output $output"
+	if [ "$mode" == off ]; then
+		echo "--off"
 	else
-		randrargs="$randrargs --output $output --mode $mode --pos $pos --rotate $rotate"
+		echo "--mode $mode"
+		echo "--pos $pos"
+		echo "${rotate:+--rotate $rotate}"
+		echo "${primary:+--primary}"
 	fi
-done <<< "`grep -v '^\s*#' $profiledir/$profile.cfg`"
-# this is ugly because of /bin/sh. I really should only support bash.
-# http://stackoverflow.com/questions/16854280/modifying-variable-inside-while-loop-is-not-remembered
-
-xrandr $randrargs
+done | xargs xrandr
