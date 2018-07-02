5a9f84549b1088d0c23f98f96c99297a7252756b
Benedikt Heine <bebe@bebehei.de>
2018-07-02 15:58:47 +0200

    [i3] Simplify xkbdtoggle.sh

--- i3/xkbdtoggle.sh
+++ i3/xkbdtoggle.sh
@@ -3,17 +3,14 @@
 # Q: But you could've solved it with an easy option in your X.org server!
 # A: Yes, but I want to bootstrap my repository without root access.
 
-KBDLIST=$HOME/.i3/kbdlist
+KBDNOW=$(setxkbmap -query | awk '/^layout:/ { print $2 }')
 
-KBDNOW=$(setxkbmap -query | awk '/^layout:/ { print $2}')
-
-cat $KBDLIST $KBDLIST \
-	| grep -A 1 "^$KBDNOW$" \
-	| grep -v $KBDNOW \
-	| cat - $KBDLIST \
-	| head -n 1 \
-	| xargs setxkbmap
-
-if [[ "us" == "$(setxkbmap -query | awk '/^layout:/ { print $2}')" ]]; then
-	xmodmap ~/.Xmodmap
-fi
+case "${KBDNOW}" in
+	us)
+		setxkbmap "de"
+		;;
+	*)
+		setxkbmap "us"
+		xmodmap ~/.Xmodmap
+		;;
+esac
