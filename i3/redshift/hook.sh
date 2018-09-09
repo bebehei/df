37c5eb73b341e8927c55f4f5192a7d015cd6127d
Benedikt Heine <bebe@bebehei.de>
2018-09-09 23:57:01 +0200

    [redshift] Handle suspend notifications better

--- i3/redshift/hook.sh
+++ i3/redshift/hook.sh
@@ -11,7 +11,13 @@ notify(){
 
 case $1 in
 	period-changed)
-		notify "Redshift" "Period changed to $3"
+		if [ "${2}" == none ]; then
+			notify "Redshift enabled"
+		elif [ "${3}" == none ]; then
+			notify "Redshift disabled"
+		else
+			notify "Redshift" "Period changed to $3"
+		fi
 		;;
 	*)
 		notify "Redshift" "Redshift just fired an unknown hook with these options: '$*'. Ah, of course, this notification is just here, to troll you! :-P"
