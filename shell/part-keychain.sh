c3edee98e0070f1bbaf00b82edd52e8e73fe6465
Benedikt Heine <bebe@bebehei.de>
2019-06-22 14:26:13 +0200

    Use KeePassXC as the ssh-agent key provider
    Using KeePassXC allows to have a smoother interface for the SSH keys.

--- shell/part-keychain.sh
+++ shell/part-keychain.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
-# array to process, either folders or directories for recursive processing
-KEYPATHS+=("$HOME/.ssh/keys")
-
+# Assert an ssh-agent (which is empty at the beginning)
 if command -v keychain 2>&1 >/dev/null; then
-	eval \
-		`find ${KEYPATHS[@]} -type f -not -name '*.pub' 2>/dev/null \
-		| xargs keychain -q --nogui --agents ssh --timeout 600 --eval`
+	eval $(keychain -q --nogui --agents ssh --timeout 600 --eval)
 fi
-unset KEYPATHS
