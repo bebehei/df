cb925c496c7e186bbe08ad735e1b666d9f11f560
Benedikt Heine <bebe@bebehei.de>
2019-08-13 10:23:32 +0200

    Remove mdo
    The command had been previously used to convert markdown on the fly into
    HTML and display it in the browser. But firefox blocked since Version
    59.
    
    https://blog.mozilla.org/security/2017/11/27/blocking-top-level-navigations-data-urls-firefox-59/

--- bin/mdo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# open a Markdown document in your browser directly
-# mdo => markdown open
-
-# e.g. preview the markdown in vim:
-# :!mdo %
-
-$BROWSER "data:text/html;base64,`markdown $* | base64 -w 0`"
