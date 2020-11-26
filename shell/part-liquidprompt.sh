eee98764300636325ceb97974870abd8a8bfe843
Benedikt Heine <bebe@bebehei.de>
2020-11-26 21:11:27 +0100

    Update liquidprompt

--- shell/part-liquidprompt.sh
+++ shell/part-liquidprompt.sh
@@ -8,5 +8,15 @@ if [ -z "${IN_NIX_SHELL}" ]; then
 	LP_ENABLE_TEMP=0
 	LP_ENABLE_BATT=0
 	LP_ENABLE_SUDO=1
+	LP_ENABLE_LOAD=1
+
+	# Show the time for commands executing longer than five seconds
+	LP_ENABLE_RUNTIME=1
+	LP_ENABLE_RUNTIME_THRESHOLD=5
+
+	# Ring the bell after termination for commands running longer than a minute
+	# It's likely that I switched away in the meantime
+	LP_ENABLE_RUNTIME_BELL=1
+	LP_ENABLE_RUNTIME_BELL_THRESHOLD=60
 	source ~/.liquidprompt/liquidprompt
 fi
