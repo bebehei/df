2e3457a8428a71d9999e8f186503f832e4c3ce55
Benedikt Heine <bebe@bebehei.de>
2026-01-18 23:45:37 +0100

    Bye bye ruby, BYE BYE PERL
    Perl, you were my first love. I won't miss you.

--- shell/part-paths.sh
+++ shell/part-paths.sh
@@ -9,17 +9,6 @@ function set_path(){
 set_path ~/.local/bin/
 set_path ~/.bin
 
-if which ruby >/dev/null 2>&1 && which gem >/dev/null 2>&1; then
-    set_path $(ruby -r rubygems -e 'puts Gem.user_dir')/bin
-fi
-
-# PERL cpan modules
-set_path $HOME/.perl/5/bin
-export PERL5LIB="$HOME/.perl/5/lib/perl5:$PERL5LIB"
-export PERL_LOCAL_LIB_ROOT="$HOME/.perl/5:$PERL_LOCAL_LIB_ROOT"
-export PERL_MB_OPT="--install_base \"$HOME/.perl/5\""
-export PERL_MM_OPT="INSTALL_BASE=$HOME/.perl/5"
-
 export GOPATH=$HOME/.go
 export GOBIN=$HOME/.gobin
 set_path $GOPATH/bin
