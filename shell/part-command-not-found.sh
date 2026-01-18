4dc33ee8fc75783aebe9131ab2c9e67be9e6e5b0
Benedikt Heine <bebe@bebehei.de>
2026-01-18 23:40:06 +0100

    Remove unused command-not-found
    I'm not on Arch anymore

--- shell/part-command-not-found.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ "${BASH_VERSION}" ] && [ -r /usr/share/doc/pkgfile/command-not-found.bash ]; then
-	. /usr/share/doc/pkgfile/command-not-found.bash
-fi
-
-if [ "${ZSH_VERSION}" ] && [ -r /usr/share/doc/pkgfile/command-not-found.zsh ]; then
-	. /usr/share/doc/pkgfile/command-not-found.zsh
-fi
