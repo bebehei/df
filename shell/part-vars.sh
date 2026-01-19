83f09b0308c3d2794d485536b3fe4e47a0555599
Benedikt Heine <bebe@bebehei.de>
2026-01-19 09:30:45 +0100

    nvim: set EDITOR variable only if installed
    Since git core.editor was set in the initial commit, I suspect I wasn't
    aware of $EDITOR in the environment. So it's safe to delete
    git/gitconfig's core.editor and take it from $EDITOR by default, which
    actually checks if nvim is installed.
    
    for git's diff.tool and merge.tool, I don't see a possibility to have a
    conditional switch and fallback to vim. I don't expect to work on
    another workstation without neovim.

--- shell/part-vars.sh
+++ shell/part-vars.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-export EDITOR=nvim
-export VISUAL=nvim
+if [ -n "$(type nvim 2>/dev/null)" ]; then
+  export EDITOR=nvim
+  export VISUAL=nvim
+else
+  export EDITOR=vim
+  export VISUAL=vim
+fi
 export BROWSER=firefox
