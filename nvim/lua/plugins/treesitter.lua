38616b9e5453c8dee7ad080f2c2f1607c3851ecc
Benedikt Heine <bebe@bebehei.de>
2026-01-25 04:35:47 +0100

    Enable tree-sitter only if CLI command installed
    If tree-sitter is not installed tree-sitter always tries to
    download+compile language support. It fails without error message and
    tries to connect to github.com on every startup.
    
    If installed, it does it once and finishes.
    
    Found out via `:checkhealth` which mocks about missing CLI command.
    
    See also: https://www.reddit.com/r/neovim/comments/128exsu/comment/jeil6ic/

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,6 +2,9 @@ return {
   'nvim-treesitter/nvim-treesitter',
   lazy = false,
   build = ':TSUpdate',
+  cond = function()
+    return vim.fn.executable 'tree-sitter' == 1
+  end,
   config = function()
     require("config.treesitter")
   end,
