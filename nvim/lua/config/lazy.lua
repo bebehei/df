c4a58a1783d62e77a994e293e65753750d058260
Benedikt Heine <bebe@bebehei.de>
2026-01-25 05:50:59 +0100

    nvim: use tokyonight theme
    lualine uses `auto` by default, inheriting the global theme.

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -18,9 +18,6 @@ require("lazy").setup({
     -- import your plugins
     { import = "plugins" },
   },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "onedark" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
   -- disable luarocks
