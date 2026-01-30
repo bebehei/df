31793521f900af19c7bc16e79b402b7cd3826ab4
Benedikt Heine <bebe@bebehei.de>
2026-01-30 11:27:59 +0100

    nvim: Create directories automatically

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,3 +4,18 @@ require("config.lazy")
 vim.opt.number = true
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
+
+-- Automatically create directories when writing to files with non-existent dirs
+vim.api.nvim_create_autocmd({ "BufWritePre", "FileWritePre" }, {
+  callback = function(event)
+    -- Skip URIs like scp://, oil://, etc.
+    if event.match:match("://") then
+      return
+    end
+
+    local dir = vim.fn.fnamemodify(event.match, ":p:h")
+    if vim.fn.isdirectory(dir) == 0 then
+      vim.fn.mkdir(dir, "p")
+    end
+  end,
+})
